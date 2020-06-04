147867abd251042b6f008edea9f361b494836c1b
wimilee <wimilee@163.com>
2020-06-05 00:39:34 +0800

    beego_first

--- src/main/helloWorld.go
+++ src/main/helloWorld.go
@@ -7,4 +7,15 @@ import "fmt"
 */
 func main() {
 	fmt.Println("HelloWorld")
+	var num int
+	fmt.Println(num)
+	var s string
+	fmt.Print(s)
+	const (
+		name string = "a"
+		age
+	)
+	fmt.Printf("%s,%T",name,name)
+	fmt.Printf("%s,%T",age,age)
+
 }
